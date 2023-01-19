37e72a64cef658e090ae0dc85c2419670421e325
Edonisgashi <edonisgashi@outlook.com>
2023-01-20 00:58:57 +0100

    Done with invoice for product's price over and under 500

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from "react";
-
+import "./App.css";
+import Header from "./Components/Header";
 const App = () => {
   const [productName, setProductName] = useState("");
   const [productQTY, setProductQTY] = useState();
   const [productPrice, setProductPrice] = useState();
   const [productDiscount, setProductDiscount] = useState(0);
   const [productVAT, setProductVAT] = useState();
-  const [priceWithoutVAT, setPriceWithoutVAT] = useState();
-  const [priceWithVAT, setPriceWithVAT] = useState();
-  const [prices, setPrices] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
-
   const [product, setProduct] = useState([]);
   const [showTable, setShowTable] = useState(false);
+  const [productOver500, setProductOver500] = useState([]);
   const correctInputs = () => {
     return (
-      (productName.length > 0 || productName !== undefined) &&
-      (productQTY > 0 || productQTY !== undefined) &&
-      (productPrice > 0 || productPrice !== undefined) &&
-      (productDiscount >= 0 || productDiscount !== undefined)
+      productName.length > 0 &&
+      productName !== undefined &&
+      productQTY > 0 &&
+      productQTY !== undefined &&
+      productPrice > 0 &&
+      productPrice !== undefined &&
+      productDiscount >= 0 &&
+      productDiscount !== undefined
     );
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPriceWithoutVAT((productPrice - productDiscount) * productQTY);
-    setPriceWithVAT(priceWithoutVAT * (productVAT / 100));
+    setShowTable(true);
+
     if (correctInputs()) {
       const productObject = {
         name: productName,
@@ -33,27 +34,49 @@ const App = () => {
         price: productPrice,
         vat: productVAT,
         discount: productDiscount,
-        priceWithoutVAT: priceWithoutVAT,
-        priceWithVAT: priceWithVAT,
+        priceWithoutVAT: (productPrice - productDiscount) * productQTY,
       };
-
-      console.log(productVAT);
-      setProduct((prevState) =>
-        prevState ? [...prevState, productObject] : [productObject]
-      );
-
-      console.log(product);
-      console.log(productObject);
+      productObject.addedVAT =
+        productObject.priceWithoutVAT * (productObject.vat / 100);
+      productObject.totalPrice =
+        productObject.priceWithoutVAT + productObject.addedVAT;
+      if (productPrice < 500) {
+        setProduct((prevState) =>
+          prevState ? [...prevState, productObject] : [productObject]
+        );
+      } else {
+        setProductOver500((prevState) =>
+          prevState ? [...prevState, productObject] : [productObject]
+        );
+        console.log(productOver500);
+      }
     }
   };
-  useEffect(() => {
-    console.log(product);
-    if (product) {
-      setShowTable(true);
-    }
-  }, [product]);
+  const allPrices = [];
+  const allVATs = [];
+  const allPricesAndVATs = [];
+  let prices = 0;
+  let VATs = 0;
+  let pricesAndVATs = 0;
+  if (product.length > 0) {
+    product.map((prd) => {
+      allPrices.push(prd.priceWithoutVAT);
+      allVATs.push(prd.addedVAT);
+      allPricesAndVATs.push(prd.totalPrice);
+    });
+    prices = allPrices.reduce((acc, el) => acc + el);
+    VATs = allVATs.reduce((acc, el) => acc + el);
+    pricesAndVATs = allPricesAndVATs.reduce((acc, el) => acc + el);
+    console.log(prices, VATs, pricesAndVATs);
+
+    console.log(allPrices);
+    console.log(allVATs);
+    console.log(allPricesAndVATs);
+  }
+
   return (
     <div>
+      <Header />
       <form
         action=""
         className="d-flex flex-column w-25 my-5 mx-auto"
@@ -63,8 +86,9 @@ const App = () => {
           <strong>Product's Name</strong>
         </label>
         <input
-          className="my-1"
+          className="my-1 form-control"
           type="text"
+          value={productName}
           placeholder="Product's name"
           id="name"
           onChange={(e) => setProductName(e.target.value)}
@@ -73,9 +97,10 @@ const App = () => {
           <strong>Product's QTY</strong>
         </label>
         <input
-          className="my-1"
+          className="my-1 form-control"
           type="number"
           min={0}
+          value={productQTY}
           placeholder="Product's QTY"
           id="qty"
           onChange={(e) => setProductQTY(Number(e.target.value))}
@@ -85,9 +110,10 @@ const App = () => {
         </label>
         <input
           type="number"
-          className="my-1"
+          className="my-1 form-control"
           id="price"
           min={0}
+          value={productPrice}
           step={0.01}
           placeholder="Product's Price"
           onChange={(e) => setProductPrice(Number(e.target.value))}
@@ -97,15 +123,15 @@ const App = () => {
         </label>
         <input
           type="number"
-          className="my-1"
+          className="my-1 form-control"
           id="discount"
+          value={productDiscount}
           min={0}
-          max={productPrice * 0.5}
           step={0.01}
           placeholder="Max value of Discount can be 50% of price"
           onChange={(e) => setProductDiscount(Number(e.target.value))}
         />
-        <div className="vat w-50 d-flex justify-content-around align-items-center my-1">
+        <div className="vat w-75 d-flex justify-content-around align-items-center my-1">
           <label htmlFor="vat">
             <strong>Product's VAT:</strong>
           </label>
@@ -125,14 +151,25 @@ const App = () => {
             onChange={(e) => setProductVAT(Number(e.target.value))}
           />{" "}
           18%
+          <input
+            type="radio"
+            name="vat"
+            value={22}
+            checked={productVAT === 22}
+            onChange={(e) => setProductVAT(Number(e.target.value))}
+          />{" "}
+          22%
         </div>
 
-        <button className="btn btn-outline-success w-50 my-3 mx-auto">
+        <button className="btn btn-outline-primary w-50 my-3 mx-auto">
           Add Product
         </button>
       </form>
       {productPrice < 500 ? (
         <table className="table table-bordered table-hover w-75 mx-auto my-5">
+          {product.map((product, i) => {
+            return <caption>Order ID : {i + 1}</caption>;
+          })}
           <thead>
             <tr>
               <th>Description</th>
@@ -150,22 +187,87 @@ const App = () => {
                     <tr key={i}>
                       <td>{product.name}</td>
                       <td>{product.qty}</td>
-                      <td>{product.price}</td>
-                      <td>{product.discount}</td>
+                      <td>${product.price}</td>
+                      <td>${product.discount}</td>
                       <td>{product.vat} %</td>
-                      {product.priceWithVAT && product.priceWithoutVAT ? (
-                        <td>
-                          {product.priceWithoutVAT} + {product.priceWithVAT} ={" "}
-                          {product.priceWithoutVAT + product.priceWithVAT}
-                        </td>
-                      ) : null}
+                      <td>
+                        {product.priceWithoutVAT.toFixed(2)} +{" "}
+                        {product.addedVAT.toFixed(2)} = $
+                        {product.totalPrice.toFixed(2)}
+                      </td>
                     </tr>
                   );
                 })
               : null}
           </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={5}>Subtotal</td>
+              <td>$ {prices.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td colSpan={5}>VAT</td>
+              <td>{VATs.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td colSpan={5}>Total to pay</td>
+              <td>$ {pricesAndVATs.toFixed(2)}</td>
+            </tr>
+          </tfoot>
         </table>
       ) : null}
+      {productOver500.length > 0
+        ? productOver500.map((product, i) => {
+            return (
+              <>
+                <table
+                  className="table table-bordered table-hover w-75 mx-auto my-5"
+                  key={i}
+                >
+                  <caption>Order ID : {i + 1}</caption>
+                  <thead>
+                    <tr>
+                      <th>Description</th>
+                      <th>QTY</th>
+                      <th>Price</th>
+                      <th>Discount</th>
+                      <th>VAT</th>
+                      <th>Total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr key={i}>
+                      <td>{product.name}</td>
+                      <td>{product.qty}</td>
+                      <td>${product.price}</td>
+                      <td>${product.discount}</td>
+                      <td>{product.vat} %</td>
+                      <td>
+                        {product.priceWithoutVAT.toFixed(2)} +{" "}
+                        {product.addedVAT.toFixed(2)} = $
+                        {product.totalPrice.toFixed(2)}
+                      </td>
+                    </tr>
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <td colSpan={5}>Subtotal</td>
+                      <td>$ {product.priceWithoutVAT.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td colSpan={5}>VAT</td>
+                      <td>{product.addedVAT.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td colSpan={5}>Total to pay</td>
+                      <td>$ {product.totalPrice.toFixed(2)}</td>
+                    </tr>
+                  </tfoot>
+                </table>
+              </>
+            );
+          })
+        : null}
     </div>
   );
 };
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark bg-opacity-75">
+        <a className="navbar-brand mx-5" href="#">
+          Invoice Calculator
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
